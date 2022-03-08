596615bc098759d35cd5f7a4de0b0611085384d0
Danila Gorelko <danila@danilax86.space>
2022-03-08 14:49:27 +0300

    add driver

--- lab1/ch_drv.c
+++ lab1/ch_drv.c
@@ -1,91 +1,73 @@
-#include <linux/module.h>
-#include <linux/version.h>
-#include <linux/kernel.h>
-#include <linux/types.h>
-#include <linux/kdev_t.h>
-#include <linux/fs.h>
-#include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
 
-static dev_t first;
-static struct cdev c_dev; 
-static struct class *cl;
+ssize_t input_char_len = 0;
+char *input_buff;
+static struct proc_dir_entry *entry;
 
-static int my_open(struct inode *i, struct file *f)
-{
-  printk(KERN_INFO "Driver: open()\n");
-  return 0;
+static ssize_t my_read(struct file *f, char __user *buf, size_t len,
+                       loff_t *off) {
+
+  int count = strlen(input_buff);
+  if (*off > 0 || len < count) {
+    return 0;
+  }
+
+  if (copy_to_user(buf, input_buff, input_char_len) != 0) {
+    return -EFAULT;
+  }
+
+  printk(KERN_DEBUG "Characters from /proc/var1: %s\n", input_buff);
+  *off = count;
+  return count;
 }
 
-static int my_close(struct inode *i, struct file *f)
-{
-  printk(KERN_INFO "Driver: close()\n");
-  return 0;
-}
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len,
+                        loff_t *off) {
 
-static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
-  printk(KERN_INFO "Driver: read()\n");
-  return 0;
-}
+  copy_from_user(input_buff, buf, len);
+  input_char_len += len - 1;
+  printk(KERN_DEBUG "Input characters amount: %ld\n", input_char_len);
 
-static ssize_t my_write(struct file *f, const char __user *buf,  size_t len, loff_t *off)
-{
-  printk(KERN_INFO "Driver: write()\n");
   return len;
 }
 
-static struct file_operations mychdev_fops =
-{
-  .owner = THIS_MODULE,
-  .open = my_open,
-  .release = my_close,
-  .read = my_read,
-  .write = my_write
+static struct file_operations mychdev_fops = {
+    .owner = THIS_MODULE,
+    .read = my_read,
+    .write = my_write
 };
- 
-static int __init ch_drv_init(void)
-{
-    printk(KERN_INFO "Hello!\n");
-    if (alloc_chrdev_region(&first, 0, 1, "ch_dev") < 0)
-	  {
-		return -1;
-	  }
-    if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
-	  {
-		unregister_chrdev_region(first, 1);
-		return -1;
-	  }
-    if (device_create(cl, NULL, first, NULL, "mychdev") == NULL)
-	  {
-		class_destroy(cl);
-		unregister_chrdev_region(first, 1);
-		return -1;
-	  }
-    cdev_init(&c_dev, &mychdev_fops);
-    if (cdev_add(&c_dev, first, 1) == -1)
-	  {
-		device_destroy(cl, first);
-		class_destroy(cl);
-		unregister_chrdev_region(first, 1);
-		return -1;
-	  }
-    return 0;
+
+static int __init ch_drv_init(void) {
+  input_buff = (char *)kmalloc(1024, GFP_KERNEL);
+  input_char_len = 0;
+  memset(input_buff, 0, 1024);
+
+  entry = proc_create("var1", 0666, NULL, &mychdev_fops);
+  printk(KERN_INFO "%s: proc file is created\n", THIS_MODULE->name);
+  return 0;
 }
- 
-static void __exit ch_drv_exit(void)
-{
-    cdev_del(&c_dev);
-    device_destroy(cl, first);
-    class_destroy(cl);
-    unregister_chrdev_region(first, 1);
-    printk(KERN_INFO "Bye!!!\n");
+
+static void __exit ch_drv_exit(void) {
+  kfree(input_buff);
+  proc_remove(entry);
+  printk(KERN_INFO "%s: proc file is deleted\n", THIS_MODULE->name);
+  printk(KERN_INFO "Bye!!!\n");
 }
- 
+
 module_init(ch_drv_init);
 module_exit(ch_drv_exit);
- 
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Danila Gorelko, Boris Novoselov");
 MODULE_DESCRIPTION("The first kernel module");
-
