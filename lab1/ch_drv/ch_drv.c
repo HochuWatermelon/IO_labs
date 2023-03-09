f14b4757c4191575e716d5677ca4144e97d71bca
Roman Glinskikh <glinskikh.roman@yandex.ru>
2023-03-09 16:57:07 +0300

    cout refactor

--- lab1/ch_drv/ch_drv.c
+++ lab1/ch_drv/ch_drv.c
@@ -37,13 +37,13 @@ static struct ListNode* curr = NULL;
 
 static int my_open(struct inode *i, struct file *f)
 {
-  printk(KERN_INFO "Driver: open()\n");
+  printk(KERN_INFO "ch_drv Driver: open()\n");
   return 0;
 }
 
 static int my_close(struct inode *i, struct file *f)
 {
-  printk(KERN_INFO "Driver: close()\n");
+  printk(KERN_INFO "ch_drv Driver: close()\n");
   return 0;
 }
 
@@ -54,7 +54,7 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
   	printk(KERN_INFO "%d, ", curr->value);
   	curr = curr->next;
   }
-  printk(KERN_INFO "Driver: read()\n");
+  printk(KERN_INFO "ch_drv Driver: read()\n");
 
   int sz = strlen(buffer);
   if (*off > 0 || len < sz)
@@ -80,7 +80,7 @@ static ssize_t my_write(struct file *f, const char __user *buf,  size_t len, lof
   }
   buffer[len] = 0;
   list_head = push_front(list_head, len);
-  printk(KERN_INFO "Driver: write()\n");
+  printk(KERN_INFO "ch_drv Driver: write()\n");
   return len;
 }
 
@@ -101,7 +101,7 @@ static struct file_operations mychdev_fops =
 
 static int ch_drv_init(void)
 {
-    printk(KERN_INFO "Device: init()\n");
+    printk(KERN_INFO "ch_drv Device: init()\n");
     if (alloc_chrdev_region(&first, 0, 1, "ch_dev") < 0)
 	  {
 		return -1;
@@ -142,7 +142,7 @@ static void ch_drv_exit(void)
     	list_head = list_head->next;
     	kfree(curr);
     }
-    printk(KERN_INFO "Device: exit()\n");
+    printk(KERN_INFO "ch_drv Device: exit()\n");
 }
 
 static struct proc_dir_entry* entry;
@@ -151,13 +151,14 @@ static char proc_buf[PROC_BUF_SIZE];
 
 static ssize_t proc_write(struct file *file, const char __user * ubuf, size_t count, loff_t* ppos)
 {
-	printk(KERN_DEBUG "Attempt to write proc file");
+	printk(KERN_DEBUG "ch_drv Attempt to write proc file");
 	return -1;
 }
 
 static ssize_t proc_read(struct file *file, char __user * ubuf, size_t count, loff_t* ppos)
 {
-    for (int i = 0; i < PROC_BUF_SIZE; i++) {
+    int i = 0;	
+    for (i = 0; i < PROC_BUF_SIZE; i++) {
       proc_buf[i] = 0;
     }
 
@@ -168,7 +169,7 @@ static ssize_t proc_read(struct file *file, char __user * ubuf, size_t count, lo
       strcat(proc_buf, mini_buf);
       curr = curr->next;
     }
-    printk(KERN_INFO "Proc: read()\n");
+    printk(KERN_INFO "ch_drv Proc: read()\n");
 
 	size_t len = strlen(proc_buf);
 	if (*ppos > 0 || count < len)
@@ -192,14 +193,14 @@ static struct proc_ops fops = {
 static int proc_example_init(void)
 {
 	entry = proc_create("var1", 0444, NULL, &fops);
-	printk(KERN_INFO "Proc: init()\n");
+	printk(KERN_INFO "ch_drv Proc: init()\n");
 	return 0;
 }
 
 static void proc_example_exit(void)
 {
 	proc_remove(entry);
-	printk(KERN_INFO "Proc: exit()\n");
+	printk(KERN_INFO "ch_drv Proc: exit()\n");
 }
 
 
---
+++ lab1/ch_drv/ch_drv.mod
@@ -0,0 +1,2 @@
+/home/omgronny/d/CSE3rdyear/io/report-skeleton/lab1/ch_drv/ch_drv.o
+
